ef12d655dd54db4e477f3cd24783252e9534eb3f
Alexsander Falcucci <alexsander.santos@luizalabs.com>
2023-05-10 00:12:08 +0200

    create custompermissions class for events
    creates a new permission class called `custompermissions` which extends `basepermission` from the rest framework library. the class contains methods to check for user authentication, user staff status and object ownership before granting permission.
    
    additionally, the class contains two new methods for `subscribe` and `unsubscribe` action that allow users to subscribe or unsubscribe from events.
    
    ```diff
    diff --git a/events/permissions.py b/events/permissions.py
    new file mode 100644
    index 000000000000..94d5fa29b157
    --- /dev/null
    +++ b/events/permissions.py
    @@ -0,0 +1,25 @@
    +from rest_framework.permissions import basepermission
    +
    +
    +class custompermissions(basepermission):
    +
    +    def has_permission(self, request, view):
    +        if not request.user.is_authenticated:
    +            return false
    +
    +        return true
    +
    +    def has_object_permission(self, request, view, obj):
    +        if request.method in ['patch', 'put'] and request.user.is_staff:
    +            return true
    +
    +        if request.method == 'delete' and obj.created_by == request.user:
    +            return true
    +
    +        if request.user.is_authenticated:
    +            return true
    +
    +        if view.action == 'subscribe' or view.action == 'unsubscribe':
    +            return true
    +
    +        return false
    ```

---
+++ events/permissions.py
@@ -0,0 +1,25 @@
+from rest_framework.permissions import BasePermission
+
+
+class CustomPermissions(BasePermission):
+
+    def has_permission(self, request, view):
+        if not request.user.is_authenticated:
+            return False
+
+        return True
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in ['PATCH', 'PUT'] and request.user.is_staff:
+            return True
+
+        if request.method == 'DELETE' and obj.created_by == request.user:
+            return True
+
+        if request.user.is_authenticated:
+            return True
+
+        if view.action == 'subscribe' or view.action == 'unsubscribe':
+            return True
+
+        return False
