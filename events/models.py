204d7f07fed38b35bf2ae180a50eb221b2c32ac2
Alexsander Falcucci <alexsander.santos@luizalabs.com>
2023-05-10 00:07:39 +0200

    adds event model
    adds an event model to the events application. this model contains fields for name, description, status, start date, end date, location, is_public, created_at, updated_at, created_by, and subscribers.
    
    the event model also contains a foreignkey and manytomanyfield to the user model, allowing for the relationship between event and user.

--- events/models.py
+++ events/models.py
@@ -1,3 +1,23 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Event(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    status = models.CharField(max_length=100)
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    location = models.CharField(max_length=100)
+    is_public = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='event_created_by'
+    )
+    subscribers = models.ManyToManyField(
+        User,
+        related_name='subscriptions',
+        blank=True
+    )
