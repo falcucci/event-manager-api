6c18d798346d96ab6db97e35c068c2df1f6e6c14
Alexsander Falcucci <alexsander.santos@luizalabs.com>
2023-05-09 16:46:49 +0200

    add rest framework status code to health endpoint
    adds the `rest_framework` `status` code to the health endpoint of the events application. this improves the readability and maintainability of the code.
    
    the changes made in this commit include:
    - adding `from rest_framework import status` to the imports
    - adding `status=status.http_200_ok` to the `httpresponse` call

--- events/health/main.py
+++ events/health/main.py
@@ -1,8 +1,10 @@
 import json
 from django.http import HttpResponse
+from rest_framework import status
 
 def health(request):
     return HttpResponse(
         json.dumps({"status": "ok"}),
-        content_type="application/json"
+        content_type="application/json",
+        status=status.HTTP_200_OK
     )
