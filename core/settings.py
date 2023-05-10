45c08745ed93ec4fb8bd9c42aad54a10134e9a93
Alexsander Falcucci <alexsander.santos@luizalabs.com>
2023-05-10 19:18:15 +0200

    add events fixtures
    adds a fixture file to the project with seven events. the fixture file contains events with a variety of information, such as the event name, description, location, start and end dates, and a list of subscribers.
    
    the diff for this commit includes changes to the `core/settings.py` file to add `localhost` to the list of allowed hosts and adding the new fixture file `fixtures/events.json` with the content of the seven events.

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-_-z4u8tprqik8-#1o8c0l2_phq8zy1iqan%k_i_i&ogcbou1yw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app', 'falcuccis.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.vercel.app', 'falcuccis.com']
 
 # Application definition
 
