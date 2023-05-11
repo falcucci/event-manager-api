e869e64ccc5f38133411059efb42b8ce49c3fa89
Alexsander Falcucci <alexsander.santos@luizalabs.com>
2023-05-12 00:46:48 +0200

    add swagger documentation to project
    adds the swagger documentation to the project. this includes adding the [drf_yasg](https://drf-yasg.readthedocs.io/en/stable/) package to `installed_apps` in `settings.py`, configuring the schema view in `urls.py` and adding an openapi info object in `urls.py` to provide a description of the api and contact information.
    
    the changes made in this commit are:
    
    * add `drf_yasg` to `installed_apps` in `settings.py`
    * configure schema view in `urls.py`
    * add openapi info object in `urls.py` to provide a description of the api and contact information

--- core/urls.py
+++ core/urls.py
@@ -17,10 +17,40 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from core.health import main
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework.permissions import AllowAny
+from django.urls import re_path
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Newsletter API",
+        default_version='v1',
+        description="API that allows us to make a CRUD of newsletters, these newsletters can be classified by tags to "
+        "improve searches. "
+        "The necessary endpoints will also be built so that common users can subscribe to the newsletters "
+        "and "
+        "unsubscribe from them. "
+        "They will only be able to subscribe to the bulletins that have reached the target that was "
+        "defined when creating the "
+        "bulletin, in case the bulletins have not reached the target they will be available so that they "
+        "can vote for them.",
+        contact=openapi.Contact(email="jacu29@gmiail.com"),
+    ),
+    public=True,
+    permission_classes=(AllowAny,),
+)
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('health/', main.health),
     path('api/', include('authentication.urls')),
     path('api/', include('events.urls')),
+
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
 ]
--- readme.md
+++ readme.md
@@ -20,8 +20,8 @@ It allows users to create, update, and delete events, as well as view and search
 - [x] Users can see a list of all events
 - [x] Users can edit the events they have created but not the ones created by other users
 - [x] Users can register to an event or un-register. This can only be done in future events and not in past events.
-- [x] Documentation of your code
-- [ ] API docs (swagger or other)
+- [x] Documentation
+- [x] API docs (swagger or other)
 - [ ] Tests
 - [ ] Add logic to manage an event capacity: if event reaches maximum number of registered attendees, an error should be returned to a user trying to register
 - [x] Add some filtering to endpoints retrieving events (e.g. date , type, status, past events, future events, etc)
