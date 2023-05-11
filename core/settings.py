7ae8a2a47d184911322d6cf8b6a4352b925f6b3f
Alexsander Falcucci <alexsander.santos@luizalabs.com>
2023-05-11 17:00:26 +0200

    adding 'timedelta' and 'simple_jwt' configs to settings.py
    adds two new variables to the settings.py file, 'timedelta' and 'simple_jwt'. the 'timedelta' variable is used to set the access token lifetime and the refresh token lifetime for the 'simple_jwt' variable.
    
    the changes made to the settings.py file include adding the 'timedelta' import statement, defining the 'simple_jwt' variable, and setting the 'access_token_lifetime' and 'refresh_token_lifetime' values.

--- core/settings.py
+++ core/settings.py
@@ -11,10 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -29,6 +29,11 @@ ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.vercel.app', 'falcuccis.com']
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000',
 ]
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1)
+}
 
 # Application definition
 
